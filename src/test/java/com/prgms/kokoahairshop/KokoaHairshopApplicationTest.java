1bfd759606b10f0c6919301796bb5e19c7503550
suy2on <tnwjdrms@naver.com>
2022-06-28 16:43:25 +0900

    ✨pull전 추가

--- src/test/java/com/prgms/kokoahairshop/KokoaHairshopApplicationTest.java
+++ src/test/java/com/prgms/kokoahairshop/KokoaHairshopApplicationTest.java
@@ -1,5 +1,14 @@
 package com.prgms.kokoahairshop;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class KokoaHairshopApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
